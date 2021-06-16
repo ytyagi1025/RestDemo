b12f4170446cf29f1aaba8863c3e5f287ed2b986
ytyagi1025 <ytyagi1025@gmail.com>
2021-06-16 15:42:51 +0530

    First commit

---
+++ RestDemo/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+	private int id;
+	private String name;
+	private int salary;
+
+}
---
+++ RestDemo/src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeRepository empRepo;
+	
+	@GetMapping("getEmployee/{id}")
+	public ResponseEntity getEmp(@PathVariable int id){
+		try {
+			Employee emp=empRepo.findById(id).get();
+			return new ResponseEntity<Employee>(emp,HttpStatus.OK);
+			
+		}catch(Exception e) {
+			return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	
+	@PostMapping("addEmployee")
+	public ResponseEntity<Object> addEmp(@RequestBody Employee emp){
+		try {
+			empRepo.save(emp);
+			return new ResponseEntity<Object>(HttpStatus.OK);
+			
+		}catch(Exception e) {
+			return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PutMapping("updateEmployee")
+	public ResponseEntity updateEmp(@RequestBody Employee employee) {
+		try {
+			Employee emp=empRepo.findById(employee.getId()).get();
+			emp.setName(employee.getName());
+			emp.setSalary(employee.getSalary());
+			empRepo.save(emp);
+			return new ResponseEntity<Object>(HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+					
+		}
+	}
+}
